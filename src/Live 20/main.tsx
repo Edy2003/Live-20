3e42757394f4f734e81bec1b98772f2f1ee50ea5
edikk <edikkopcha6@gmail.com>
2023-09-25 15:07:21 +0300

    Fixing bugs

--- src/Live 20/main.tsx
+++ src/Live 20/main.tsx
@@ -2,22 +2,61 @@ import './style.css'
 import {useEffect, useState} from "react";
 
 function Main (){
-    const [numb,setNumb]=useState<number>();
-    const [arr,setArr]=useState<number[]>([0,0,0,0,0]);
+    const [number,setNumber]=useState<number>(0);
+    const [arr,setArr]=useState<number[]>([]);
     const [correctArr,setCorrectArr]=useState<number[]>([]);
     const [result,setResult]=useState<string>('');
+    const [message,setMessage]=useState<string>('');
+    const [passedStatus,setPassedStatus]=useState<boolean>(true);
+    const [level,setLevel]=useState<string>('5');
+    const [endedGame,setEndedGame]=useState<boolean>(true)
+
+    function restartGame(){
+        setLevel('5')
+        setCorrectArr([]);
+        setArr([]);
+        setEndedGame(true);
+        setNumber(0);
+        setMessage('');
+        setResult('');
+    }
+
+    function generateArr() {
+        if(endedGame) {
+            switch (level) {
+                case '5':
+                    setArr([0, 0, 0, 0, 0]);
+                    break;
+                case '10':
+                    setArr([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+                    break;
+                case '15':
+                    setArr([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+                    break;
+            }
+        }else{
+            setMessage('Wait until game ends')
+        }
+        setEndedGame(false);
+    }
 
     function generateNumb(){
-        setNumb(Math.floor(Math.random()*10))
+        passedStatus?setNumber(Math.floor(Math.random()*700)):setMessage('Choose button!!!');
+        setPassedStatus(false);
     }
+
     function changeValue(e:number,index:number,el:any){
-        if (numb != null) {
-            arr[index] = numb;
-        }
-        setCorrectArr([...correctArr,arr[index]])
-        el.target.disabled = true;
-        setArr([...arr]);
-        checkArr();
+      if(passedStatus){
+          setMessage('Generate new number!!!')
+      }else {
+          if (number != null) {
+              arr[index] = number;
+          }
+          setCorrectArr([...correctArr, arr[index]])
+          el.target.disabled = true;
+          setArr([...arr]);
+          setPassedStatus(true);
+      }
     }
     function checkArr(){
         correctArr.sort((a,b)=>a > b? 1:-1);
@@ -30,23 +69,36 @@ function Main (){
             }
         }else{}
     }
+
     useEffect(()=>{
         checkArr();
     },[arr])
     return(
         <div className='container'>
             <div>
+                {endedGame?<div>
+                        <select onChange={(e)=>setLevel(e.target.value)}>
+                            <option value={'5'}>5</option>
+                            <option value={'10'}>10</option>
+                            <option value={'15'}>15</option>
+                        </select>
+                        <button onClick={generateArr}>Generate array</button>
+                    </div>
+                    :
+                    <button onClick={restartGame}>Restart Game</button>
+                }
+
                 <ul>
                     {arr.map((e,index)=>{
-                        return <li key={index} ><button onClick={(n)=>changeValue(e,index,n)}>{e}</button></li>
+                        return <li key={index}><button onClick={(n)=>changeValue(e,index,n)}>{e}</button></li>
                     })}
                 </ul>
             </div>
             <div className='result'>
-                {result}
+                {arr.length>1&&correctArr.length === arr.length?result:message}
             </div>
             <div className='numbContainer'>
-                <div className='currentNumb'>{numb}</div>
+                <div className='currentNumb'>{arr.length === correctArr.length? '':number}</div>
                 <button className='currentNumbBtn' onClick={generateNumb}>Generate new number</button>
             </div>
         </div>
